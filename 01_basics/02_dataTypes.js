d303f2db7a4ced70e9efd9a2d8dffdea85e34e65
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-01-28 14:04:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+// "use strict";
+
+// let a = 123;
+// let b = "abc";
+// let c = null;
+// let d = 123.123;
+// let e = undefined;
+// let f = function add(a, b) {
+//   c = a + b;
+//   return c;
+// };
+
+// console.table([typeof a, typeof b, typeof c, typeof d, typeof e, typeof f]);
+
+let name="tushar"
+let age=26
+let isLoggedIn=false
+
+//number=>2 to power 53
+//bigint
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+//undefined=>
+//symbol=>
+
+
+// object
+console.log(typeof undefined)
+console.log(typeof null)
+
