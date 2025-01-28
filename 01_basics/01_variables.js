63a47e5451e1b0a7a5ffe58ff66f4777d962911a
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-01-28 14:03:47 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=14356
+let accountEmail="abc@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+let accountState;
+
+
+// accountId=2
+accountEmail="bca@gmail.com"
+accountPassword="2121212"
+accountCity="indore"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+
+/*
+do not use var to declare variables
+*/
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
