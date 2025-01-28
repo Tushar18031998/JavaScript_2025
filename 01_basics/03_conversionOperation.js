4cbce439a79b23bffef78b9ef6e7881d00fdd9fd
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-01-28 16:53:04 +0000

    convertion operation of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,75 @@
+// let score="33"
+
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+
+// let score=null
+
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// let score=undefined
+
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// let score=true
+
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+// let score="tushar"
+
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+
+// let isLoggedIn=1
+
+// let boolenIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof(isLoggedIn))
+// console.log(typeof(boolenIsLoggedIn))
+// console.log(boolenIsLoggedIn)
+
+// let isLoggedIn="h"
+
+// let boolenIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof(isLoggedIn))
+// console.log(typeof(boolenIsLoggedIn))
+// console.log(boolenIsLoggedIn)
+
+/***********************
+//1=> true;
+//0=>false
+//""=>false
+//"tushar"=>true
+************************/
+
+
+// let someNumber=33
+
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+
+// console.log(typeof(stringNumber))
