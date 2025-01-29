a19a3bd7a7d6df5883511a2728b08e789a9821cf
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-01-29 18:11:25 +0000

    number and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,43 @@
+// const score=400
+
+// const balance=new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+// const otherNumber=23.675
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++++++++++++++++++++++   MATHS        ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,3,5,6,7,3,8))
+// console.log(Math.max(4,3,5,6,7,3,8))
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*100)+1);
+console.log((Math.random()*1000)+1);
+console.log(Math.floor(Math.random()*10000)+1);
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min-1))+min)
+
+
+
+
+// const randomNum = Math.random() * 9000
+// const token = Math.floor(1000 + randomNum)
+// console.log(token)
