d451d5e58207819b48b1a2edd0e2e9f7213b270a
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-01-29 11:00:28 +0000

    datatypes extra

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -29,3 +29,43 @@ let isLoggedIn=false
 console.log(typeof undefined)
 console.log(typeof null)
 
+//************************Datatypes
+
+// two types of datatypes
+
+//1. primitive
+
+//7types: String,number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIN = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id===anotherId)
+
+const bigNumber=3444444444444n
+
+
+//2.Reference Type (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros=["shaktiman","nagraj","doga"]
+
+let myobj={
+    name:"tushar",
+    age: 22,
+}
+
+
+const myFunction=function(){
+    console.log("hello world")
+}
+
+console.log(typeof bigNumber)
