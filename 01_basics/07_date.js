97d286c653e393d1a4334d2541c54c88b2f5f91e
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-02-06 18:14:10 +0000

    date and time

---
+++ 01_basics/07_date.js
@@ -0,0 +1,38 @@
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate= new Date(2023,0,23)
+// console.log(myCreateDate.toDateString())
+
+// let myCreateDate= new Date(2023,0,23,5,3)
+// console.log(myCreateDate.toLocaleString())
+
+// let myCreateDate= new Date("2023-01-14")
+// let myCreateDate= new Date("01-14-2024")
+
+// let myCreateDate= new Date("01-14-2024")
+// console.log(myCreateDate.toLocaleString())
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+// console.log(myCreateDate.getTime);
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+
+// console.log(`the day of month is ${newDate.getDay()} and the current Time is ${Math.floor(newDate.getTime()/1000)}`)
+
+
+// newDate.toLocaleString('default',{
+//     weekday:"long"
+// })
+
+// console.log(newDate)
