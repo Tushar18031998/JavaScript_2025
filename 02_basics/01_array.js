ca4abc06bfeadbde388c9da706daa5193b027fe3
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-02-06 18:14:27 +0000

    Arrays 1st part

---
+++ 02_basics/01_array.js
@@ -0,0 +1,67 @@
+// let arr =new Array
+
+// arr.push(1,2,3)
+
+// console.log(arr)
+
+const arr1 = [1, 2, 4, 5, 6, 7, 8, 9, true, "tushar"];
+
+// console.log(arr1);
+
+arr1.push(1, 3, 4);
+// console.log(arr1);
+
+arr1.pop();
+// console.log(arr1);
+
+// console.log(arr1[9]);
+
+const myHerose = ["shaktiman", "benton"];
+
+const arr2 = new Array(1, 2, 3, 4, 5);
+
+// console.log(arr2[2]);
+
+
+
+
+//****************************************************ARRAY METHODS */
+
+const myArr=[1,2,3,4,5,6]
+
+// myArr.push(6)
+// myArr.push(7)
+
+// myArr.pop()
+
+// myArr.unshift(0)
+// myArr.shift()
+
+
+// console.log(myArr)
+
+//  console.log(myArr.includes(9));
+//  console.log(myArr.indexOf(9));
+//  console.log(myArr.indexOf(3));
+//  console.log(myArr.includes(6));
+
+
+// const newArr=myArr.join()
+
+// console.log(myArr)
+// console.log(newArr)
+
+//*************************************SLICE AND SPLICE */
+
+
+console.log("A ",myArr)
+
+const myn1=myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("C ",myArr)
+
+console.log(myn2);
