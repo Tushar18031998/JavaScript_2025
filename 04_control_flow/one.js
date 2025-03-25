7ea6676388658f54d64bf23a528f9a92955227d5
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-03-25 09:26:55 +0000

    AND operator OR

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+//if
+
+// const isUserloggedIn=true
+
+// if(isUserloggedIn){
+
+// }
+// if(false){
+
+// }
+
+//     <,>,<=,>=,==,!=,===,!==
+
+// if (2==="2"){
+//     console.log("executed")
+// }else {
+//     console.log("else executed")
+
+// }
+
+//short hgand notaion
+
+const balance = 760;
+
+// if(balance>500) console.log("test");
+
+// if (balance < 500) {
+//   console.log("less than 500");
+// } else if (balance < 750) {
+//   console.log("less than 750");
+// } else if (balance < 900) {
+//   console.log("less than 900");
+// } else {
+//   console.log("grater than 1000");
+// }
+
+const isUserloggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+const guestuser=true
+
+if (isUserloggedIn && debitCard && 2==3) {
+console.log("allow to buy cources");
+
+}
+if(loggedInFromEmail||loggedInFromEmail|| guestuser){
+    console.log("user logged in");
+    
+}
