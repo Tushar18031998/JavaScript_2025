e9d9f194072fb5fd90cf030964115d42f6dda675
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-03-25 09:27:12 +0000

    switch cases

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,67 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan";
+
+
+switch (month){
+    case "jan":
+        console.log("january");
+        
+        break;
+    case "feb":
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+    case 5:
+        console.log("may");
+        
+        break;
+    case 6:
+        console.log("june");
+        
+        break;
+    case 7:
+        console.log("july");
+        
+        break;
+    case 8:
+        console.log("augest");
+        
+        break;
+    case 9:
+        console.log("sept");
+        
+        break;
+    case 10:
+        console.log("oct");
+        
+        break;
+    case 11:
+        console.log("nov");
+        
+        break;
+    case 12:
+        console.log("dec");
+        
+        break;
+
+    default:
+        console.log("not valid month");
+
+        break;
+}
