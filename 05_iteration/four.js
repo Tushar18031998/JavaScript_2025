3f85ec0b066acaddfdff63c5b66d46a142232bc8
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-03-25 09:29:58 +0000

    how to iterate objects

---
+++ 05_iteration/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+  js: "javascript",
+  cpp: "c++",
+  rb: "suby",
+  swift: "swift by apple",
+};
+
+// for (const key in myObject) {
+//   console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+
+// }
+
+const map = new Map();
+map.set("In", "India");
+map.set("usa", "united states of America");
+map.set("fr", "france");
+map.set("fr", "france");
+
+//maps are not iterable in this way
+// for (const key in map) {
+//   console.log(key);
+// }
