14ad5ad8028f52e696630a167bec0c013412c883
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-03-25 09:24:29 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,53 @@
+// // var c=300
+// let a = 300;
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   console.log("inner:", a);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//   const username = "Tushar";
+
+//   function two() {
+//     const website = "youtube";
+//     console.log(username);
+//   }
+// //   console.log(website);
+//   two();
+// }
+// one();
+
+
+if(true){
+    const username="tushar"
+    if(username==="tushar "){
+        const website="youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username)
+
+
+/**********************Interesting question */
+addone(5)
+function addone(num){
+return num+1
+}
+
+
+
+
+
+const addTwo=function(num){
+    return num+2
+}
+
+addTwo(5)
