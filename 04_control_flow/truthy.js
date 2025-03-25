9f290f558f0b894312584b93ef1bb45c6ccda828
TUSHAR BHARAMBE <tushar18bharambe@gmail.com>
2025-03-25 09:27:33 +0000

    truthy falsy value

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,47 @@
+// const userEmail="Tshar@gmail.com"
+
+// if(userEmail){
+// console.log("got user email");
+// }
+// else{
+//     console.log("Dont have user email")
+// }
+
+
+// const userEmail=[]
+
+// if (userEmail.length===0) {
+//     console.log("array is empty"); 
+// }
+
+
+const emptyObject={}
+
+if(Object.keys(emptyObject).length===0){
+    console.log("object is empty")
+}
+
+
+
+
+let val1;
+// val1=5??10
+// val1=null??10
+
+// var1=undefined??15
+
+// val1 =null??10??20
+
+// console.log(val1)
+
+
+//ternary opearator
+
+// 
+// condition? true:false
+
+
+
+const iceTeaPrice=100
+
+iceTeaPrice <=80 ? console.log("less than 80"):console.log("more than 80")
